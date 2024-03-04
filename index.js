a7769bc6290f07d627b09f44ea0daf6a43cbb10e
jstro1836 <39538412+jstro1836@users.noreply.github.com>
2024-03-04 14:33:51 -0600

    Submitting block 7 assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+//Pseudo code for Block 7 is below
+
+/*
+Step 1: Declare three constant numeric variables assigned the values of 10, 40, and 39 using arithmentic operators
+Step 2: Declare a string variable saying "You have received this message because you have been chosen to open an important vault"
+Step 3: Create a pop up dialog that displays the string and numberic values
+*/
+
+// Declaring the constant variable num1 and using multiplication to assign a value of 10 
+const num1 = 5 * 2; 
+
+// Declaring the constant variable num2 and using division to assign a value of 40 
+const num2 = 400 / 10; 
+
+// Declaring the constant variable num3 and using addition to assign a value of 39 
+const num3 = 25 + 14; 
+
+// Declaring the constant variable userMessage and assigning string text 
+const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "; 
+
+// Creating a popup dialog that alerts the user to open the vault providing the secret codes 10, 40, 39
+alert(userMessage + num1 + "-" + num2 + "-" + num3);
+
+
